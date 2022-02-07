1d6d5afb9218489d0e93ae25554538f42c66427b
BobCollinNovi <bob.collin@novi-education.nl>
2022-02-07 16:22:17 +0100

    Opdracht1

--- Oefening1.java
+++ Oefening1.java
@@ -9,20 +9,41 @@ public class Oefening1 {
         int aantal = scanner.nextInt();
         int targetSum = scanner.nextInt();
 
-        int[] arr = new int[aantal];
-        int i = 0;
+        int[] myList = new int[aantal];
+        int x = 0;
 
         while (scanner.hasNext()) {
             if (scanner.hasNextInt()) {
-                arr[i] = scanner.nextInt();
-                i++;
+                myList[x] = scanner.nextInt();
+                x++;
             }
         }
-
+        System.out.println(Arrays.toString(myList));
         // Print de inhoud van de array (check dat het werkt)
         // Deze regel kun je verwijderen
-        System.out.println(Arrays.toString(arr));
-        
+
+//        System.out.println(Arrays.toString(arr));
+
         // Implementeer jouw algoritme hier.
+        int q = 0;
+        //int[] myList = new int[] { -4, -1, -1, 0, 1, 2 };
+        //int[] myList = new int[] { 6, 0, -1, 0, 1, 2, -1, -4 };
+        //int[] myList = new int[] { 1, -1, 0, 2, 3 };
+        //Arrays.sort(myList);
+
+        int totaal = targetSum;
+        System.out.println(totaal);
+        for (int i = 0; i < myList.length; i++) {
+            for (int j = i + 1; j < myList.length; j++) {
+                for (int k = j + 1; k < myList.length; k++) {
+                    if (myList[i] + myList[j] + myList[k] == totaal) {
+                        q += 1;
+                        //System.out.println(myList[i] + "+" + myList[j] + "+" + myList[k]);
+                    }
+                }
+            }
+        }
+        System.out.println(q);
     }
+
 }
